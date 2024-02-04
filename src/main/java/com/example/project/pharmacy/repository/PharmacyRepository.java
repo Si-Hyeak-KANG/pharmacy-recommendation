2bf856a2d6c538c6cd2de9fa1b464d3470274cda
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-02-04 18:00:27 +0900

    testcontainer 적용

---
+++ src/main/java/com/example/project/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,9 @@
+package com.example.project.pharmacy.repository;
+
+import com.example.project.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+}
